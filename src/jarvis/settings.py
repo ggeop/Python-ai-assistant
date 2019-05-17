f76247b7fba4d3c3964f24422178a2765a59d36a
George Papa <giorgos.papachristou@commsquare.com>
2019-05-17 11:19:35 +0300

    Update settings.py

--- src/jarvis/settings.py
+++ src/jarvis/settings.py
@@ -1,11 +1,13 @@
 
 # Trigger words
-ASSISTANT_TRIGGERING_WORD = 'hello'
-BROWSER_TRIGGERING_WORD = 'open'
-SENT_EMAIL_TRIGGERING_WORD = 'email'
-LAUNCH_APPLICATION = 'launch'
-TELL_TIME = 'time'
-TELL_ME_ABOUT = 'tell me about'
+TRIGGERING_WORDS ={
+    'enable_jarvis' : 'hello'
+    'open_browser' : 'open'
+    'sent_email' : 'email'
+    'launch_application' : 'launch'
+    'tell_time' : 'time'
+    'tell_about' : 'tell me about'
+}
 
 # Google API Speech recognition settings
 AMBIENT_DURATION = 0.5
