b0fb581ef837c61a5dd397a39e98f7270b9dea7c
geop papa <geopapa@testuser.com>
2019-05-18 20:10:04 +0300

    Add docstrings in class methods.

--- src/jarvis/action_manager.py
+++ src/jarvis/action_manager.py
@@ -13,6 +13,10 @@ class ActionController:
 
     @classmethod
     def open_website_in_browser(cls, words):
+        """
+        Opens a web page in the browser.
+        :param words: string (e.g Open the site youtube)
+        """
         reg_ex = re.search(TRIGGERING_WORDS['open_browser'] + ' (.+)', words)
         if reg_ex:
             domain = reg_ex.group(1)
@@ -24,6 +28,11 @@ class ActionController:
 
     @classmethod
     def _create_url(cls, domain):
+        """
+        Creates a url. It checks if there is .com suffix and add it if it not exist.
+        :param domain: string (e.g youtube)
+        :return: string (e.g http://www.youtube.com)
+        """
         if re.search('.com', domain):
             url = 'http://www.' + domain
         else:
@@ -32,6 +41,10 @@ class ActionController:
 
     @classmethod
     def tell_the_weather(cls, words):
+        """
+        Tells the weather of a place
+        :param words: string (e.g weather in London)
+        """
         reg_ex = re.search('weather in (.*)', words)
         if reg_ex:
             city = reg_ex.group(1)
@@ -45,11 +58,18 @@ class ActionController:
 
     @classmethod
     def tell_the_time(cls):
+        """
+        Tells ths current time
+        """
         now = datetime.now()
         assistant_response('Current time is %d hours %d minutes' % (now.hour, now.minute))
 
     @classmethod
     def tell_me_about(cls, words):
+        """
+        Tells about something by searching in wikipedia
+        :param words: string (e.g about google)
+        """
         reg_ex = re.search('about (.*)', words)
         try:
             if reg_ex:
