357855a4b07f264379792e434a768a1dd18e191e
geop papa <geopapa@testuser.com>
2019-05-19 14:00:06 +0300

    Enrich settings with more configuration settings in speech_recognition

--- src/jarvis/settings.py
+++ src/jarvis/settings.py
@@ -1,6 +1,7 @@
 
 # General assistant settings
 GENERAL_SETTINGS = {
+    'assistant_name': 'Jarvis',
     'response_in_speech': False,
     'response_in_text': True
 }
@@ -18,9 +19,12 @@ TRIGGERING_WORDS = {
 }
 
 # Google API Speech recognition settings
+# SpeechRecognition: https://pypi.org/project/SpeechRecognition/2.1.3
 SPEECH_RECOGNITION = {
     'ambient_duration': 0.1,
-    'pause_threshold': 1
+    'pause_threshold': 1,  # minimum length silence (in seconds) at the end of a sentence
+    'energy_threshold': 4000,  # microphone sensitivity, for loud places, the energy level should be up to 4000
+    'dynamic_energy_threshold': False  # For unpredictable noise levels
 }
 
 # Google text to speech API settings
