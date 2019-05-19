b15445f17b9d255e1177fcd07e3fcc0cd0bddc73
geop papa <geopapa@testuser.com>
2019-05-19 21:02:59 +0300

    Refactoring: Update the tests

--- src/tests/command_controller_tests.py
+++ src/tests/command_controller_tests.py
@@ -1,15 +1,41 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from jarvis.command_manager import CommandController
+from jarvis.command_manager import CommandManager
 from jarvis.settings import TRIGGERING_WORDS
 
 
 class CommandControllerTests(unittest.TestCase):
 
-    @patch('jarvis.action_controller.subprocess.Popen')
-    def test_execute_commands(self, mocked_Popen):
+    @classmethod
+    def setUpClass(self):
+        self.command_controller = CommandManager()
+
+    @patch('jarvis.command_manager.CommandManager._record')
+    @patch('jarvis.command_manager.sr.Microphone')
+    @patch('jarvis.command_manager.sr.Recognizer.recognize_google')
+    @patch('jarvis.command_manager.CommandManager._get_words')
+    def test_wake_up(self, mocked_get_words, mocked_recognize_google, mocked_microphone, mocked_record):
+        mocked_recognize_google.return_value = 'can you '
+        self.assertEqual(False, self.command_controller.wake_up_check())
+
+        mocked_recognize_google.return_value = 'can you ' + TRIGGERING_WORDS['enable_jarvis']
+        self.assertEqual(True, self.command_controller.wake_up_check())
+
+    @patch('jarvis.action_manager.ActionManager.open_website_in_browser')
+    @patch('jarvis.command_manager.CommandManager._record')
+    @patch('jarvis.command_manager.sr.Microphone')
+    @patch('jarvis.action_manager.subprocess.Popen')
+    def test_execute_commands(self, mocked_Popen, mocked_microphone, mocked_record, mocked_open_website):
+
+        commands = {}
+        self.command_controller._execute_commands(commands)
+        self.assertEqual(0, mocked_Popen.call_count)
+
         commands = {TRIGGERING_WORDS['open_browser']}
-        words = 'open youtube'
-        CommandController._execute_commands(commands, words)
+        self.command_controller.words = 'open youtube'
+        self.command_controller._execute_commands(commands)
         self.assertEqual(1, mocked_Popen.call_count)
+
+
+
