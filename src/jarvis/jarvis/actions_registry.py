a5d7e90480e2378b690529a5178522af1b06e2bb
George Papa <giorgos.papachristou@commsquare.com>
2019-05-24 15:07:18 +0300

    Update actions_registry.py

--- src/jarvis/jarvis/actions_registry.py
+++ src/jarvis/jarvis/actions_registry.py
@@ -1,15 +1,21 @@
 from jarvis.action_manager import ActionManager
 
 
-# All available assistant actions
+"""
+All available assistant actions
+Keys description:
+    - 'enable': boolean (With True are the enabled actions)
+    - 'action': The action method in ActionManager
+    - 'tags': The available triggering tags
+"""
 ACTIONS = {
     'open_browser': {'enable': True,
                      'action': ActionManager.open_website_in_browser,
-                     'tags': set('open', 'do')},
+                     'tags': set('open')},
 
     'tell_time': {'enable': True,
                   'action': ActionManager.tell_the_time,
-                  'tags': set('time')},
+                  'tags': set('time', 'hour')},
 
     'tell_about': {'enable': True,
                    'action': ActionManager.tell_me_about,
@@ -20,9 +26,10 @@ ACTIONS = {
                         'tags': set('weather')},
 }
 
+
 CONTROL_ACTIONS = {
     'enable_jarvis': {'action': ActionManager.enable_jarvis,
-                      'tags': set('start', 'hi', 'jarvis')},
+                      'tags': set('start', 'hi', 'hello', 'jarvis')},
 
     'disable_jarvis': {'action': ActionManager.disable_jarvis,
                        'tags': set('stop', 'shut down')},
