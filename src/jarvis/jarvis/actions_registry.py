e0aff51fcb059c8ae98ef52bfe3dbe87ee8e749c
George Papa <giorgos.papachristou@commsquare.com>
2019-05-24 10:55:24 +0300

    Update actions_registry.py

--- src/jarvis/jarvis/actions_registry.py
+++ src/jarvis/jarvis/actions_registry.py
@@ -1,24 +1,29 @@
 from jarvis.action_manager import ActionManager
-from collections import OrderedDict
 
 
 # All available assistant actions
-ACTIONS = OrderedDict({
+ACTIONS = {
+    'open_browser': {'enable': True,
+                     'action': ActionManager.open_website_in_browser,
+                     'tags': set('open', 'do')},
+
+    'tell_time': {'enable': True,
+                  'action': ActionManager.tell_the_time,
+                  'tags': set('time')},
+
+    'tell_about': {'enable': True,
+                   'action': ActionManager.tell_me_about,
+                   'tags': set('about')},
+
+    'current_weather': {'enable': True,
+                        'action': ActionManager.tell_the_weather,
+                        'tags': set('weather')},
+}
+
+CONTROL_ACTIONS = {
     'enable_jarvis': {'action': ActionManager.enable_jarvis,
-                      'tags': ['start', 'hi', 'jarvis']},
-
-    'open_browser': {'action': ActionManager.open_website_in_browser,
-                     'tags': ['open', 'do']},
-
-    'tell_time': {'action': ActionManager.tell_the_time,
-                  'tags': ['time']},
-
-    'tell_about': {'action': ActionManager.tell_me_about,
-                   'tags': ['about']},
-
-    'current_weather': {'action': ActionManager.tell_the_weather,
-                        'tags': ['weather']},
+                      'tags': set('start', 'hi', 'jarvis')},
 
     'disable_jarvis': {'action': ActionManager.disable_jarvis,
-                       'tags': ['stop', 'shut down']},
-})
+                       'tags': set('stop', 'shut down')},
+}
