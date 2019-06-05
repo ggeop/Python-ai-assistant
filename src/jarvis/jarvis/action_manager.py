e163d52e2307e4c213d672cf7e4244cd483fbf13
geop papa <geopapa@testuser.com>
2019-06-05 21:59:52 +0300

    -Actions refactoring -Change in assistant response messages - Add try-except - Create open_in_youtube action

--- src/jarvis/jarvis/action_manager.py
+++ src/jarvis/jarvis/action_manager.py
@@ -5,14 +5,47 @@ import psutil
 import subprocess
 import wikipedia
 import logging
+import requests
 from pyowm import OWM
 from datetime import datetime
+from bs4 import BeautifulSoup as bs
 
 from jarvis.settings import WEATHER_API
 from jarvis.assistant_utils import assistant_response
 
 
 class ActionManager:
+
+    @staticmethod
+    def enable_jarvis(**kwargs):
+        """
+        Creates the assistant respond according to the datetime hour and
+        updates the execute state.
+        """
+        now = datetime.now()
+        day_time = int(now.strftime('%H'))
+
+        if day_time < 12:
+            assistant_response('Good morning human')
+        elif 12 <= day_time < 18:
+            assistant_response('Good afternoon human')
+        else:
+            assistant_response('Good evening human')
+        assistant_response('What do you want to do for you?')
+
+        return {'ready_to_execute': True,
+                'enable_time': now}
+
+    @staticmethod
+    def disable_jarvis(**kargs):
+        """
+        Shutdown the assistant service
+        :param args:
+        :return:
+        """
+        assistant_response('Bye bye!!')
+        logging.debug('Application terminated gracefully.')
+        sys.exit()
 
     @classmethod
     def open_website_in_browser(cls, tag, voice_transcript, action):
@@ -26,12 +59,16 @@ class ActionManager:
         and execute only the first one, in our case will open the youtube.
         """
         reg_ex = re.search(tag + ' ([a-zA-Z]+)', voice_transcript)
-        if reg_ex:
-            domain = reg_ex.group(1)
-            url = cls._create_url(domain)
-            assistant_response('Sure')
-            subprocess.Popen(["python", "-m",  "webbrowser",  "-t",  url], stdout=subprocess.PIPE)
-            assistant_response('I opened the {0}'.format(domain))
+        try:
+            if reg_ex:
+                domain = reg_ex.group(1)
+                url = cls._create_url(domain)
+                assistant_response('Sure')
+                subprocess.Popen(["python", "-m",  "webbrowser",  "-t",  url], stdout=subprocess.PIPE)
+                assistant_response('I opened the {0}'.format(domain))
+        except Exception as e:
+            logging.debug(e)
+            assistant_response("I can't find this domain '{0}'".format(domain))
 
     @classmethod
     def _create_url(cls, tag):
@@ -54,15 +91,23 @@ class ActionManager:
         :param voice_transcript: string (e.g 'weather in London')
         """
         reg_ex = re.search(tag + ' in ([a-zA-Z]+)', voice_transcript)
-        if reg_ex:
-            city = reg_ex.group(1)
-            owm = OWM(API_key=WEATHER_API['key'])
-            obs = owm.weather_at_place(city)
-            w = obs.get_weather()
-            k = w.get_status()
-            x = w.get_temperature(WEATHER_API['unit'])
-            assistant_response('Current weather in %s is %s. The maximum temperature is %0.2f and the minimum '
-                               'temperature is %0.2f degree celcius' % (city, k, x['temp_max'], x['temp_min']))
+        try:
+            if reg_ex:
+                if WEATHER_API['key']:
+                    city = reg_ex.group(1)
+                    owm = OWM(API_key=WEATHER_API['key'])
+                    obs = owm.weather_at_place(city)
+                    w = obs.get_weather()
+                    k = w.get_status()
+                    x = w.get_temperature(WEATHER_API['unit'])
+                    assistant_response('Current weather in %s is %s. The maximum temperature is %0.2f and the minimum '
+                                       'temperature is %0.2f degree celcius' % (city, k, x['temp_max'], x['temp_min']))
+                else:
+                    assistant_response("Weather forecast is not working.\n"
+                                       "You can get an Weather API key from: https://openweathermap.org/appid")
+        except Exception as e:
+            logging.debug(e)
+            assistant_response("I faced an issue with the weather site..")
 
     @staticmethod
     def tell_the_time(**kwargs):
@@ -86,13 +131,14 @@ class ActionManager:
                 response = cls._decoded_wiki_response(topic)
                 assistant_response(response)
         except Exception as e:
-            logging.error(e)
+            logging.debug(e)
+            assistant_response(" I can't find on the internet what you want")
 
     @classmethod
     def _decoded_wiki_response(cls, topic):
         """
         A private method for decoding the wiki response.
-        :param topic:
+        :param topic: string
         :return:
         """
         ny = wikipedia.page(topic)
@@ -134,26 +180,6 @@ class ActionManager:
         assistant_response('I opened a new impress document..')
 
     @staticmethod
-    def enable_jarvis(**kwargs):
-        """
-        Creates the assistant respond according to the datetime hour and
-        updates the execute state.
-        """
-        now = datetime.now()
-        day_time = int(now.strftime('%H'))
-
-        if day_time < 12:
-            assistant_response('Good morning human')
-        elif 12 <= day_time < 18:
-            assistant_response('Good afternoon human')
-        else:
-            assistant_response('Good evening human')
-        assistant_response('What do you want to do for you?')
-
-        return {'ready_to_execute': True,
-                'enable_time': now}
-
-    @staticmethod
     def tell_memory_consumption(**kwargs):
         """
         Responds the memory consumption of the assistant process
@@ -164,12 +190,25 @@ class ActionManager:
         assistant_response('I use {} GB..'.format(memoryUse))
 
     @staticmethod
-    def disable_jarvis(**kargs):
+    def open_in_youtube(tag, voice_transcript, **kwargs):
         """
-        Shutdown the assistant service
-        :param args:
-        :return:
+        Open a video in youtube.
+        :param tag: string (e.g 'tdex')
+        :param voice_transcript: string (e.g 'open in youtube tdex')
         """
-        assistant_response('Bye bye!!')
-        logging.debug('Application terminated gracefully.')
-        sys.exit()
+        # TODO:  Replace with YOUTUBE API
+        reg_ex = re.search(tag + ' ([a-zA-Z]+)', voice_transcript)
+        try:
+            if reg_ex:
+                search_text = reg_ex.group(1)
+                base = "https://www.youtube.com/results?search_query=" + "&orderby=viewCount"
+                r = requests.get(base + search_text.replace(' ', '+'))
+                page = r.text
+                soup = bs(page, 'html.parser')
+                vids = soup.findAll('a', attrs={'class': 'yt-uix-tile-link'})
+                video = 'https://www.youtube.com' + vids[0]['href']
+                subprocess.Popen(["python", "-m", "webbrowser", "-t", video], stdout=subprocess.PIPE)
+        except Exception as e:
+            logging.debug(e)
+            assistant_response("I can't find what do you want in Youtube..")
+
