159262da37812b7803d1878c70b5563e9f1e28f9
geo <geo>
2019-06-11 20:58:35 +0300

    Remove comments

--- src/jarvis/jarvis/core/processor.py
+++ src/jarvis/jarvis/core/processor.py
@@ -15,22 +15,16 @@ class Processor:
     def _process(self):
         # Check if the assistant is waked up
         if self.controller.wake_up_check():
-            # Record user voice and create a voice transcipt
-            self.controller.get_transcript()
 
-            # Extract actions and update the actions (state of action controller)
+            self.controller.get_transcript()
             self.controller.get_skills()
 
-            # Checks is there is an action to execute them
             if self.controller.skills_to_execute:
                 self.controller.execute()
             else:
-                # Check for shutdown
                 self.controller.shutdown_check()
 
-                # If there is not an action the assistant make a request in
-                # WolframAlpha api for response
+                # If there is not an action the assistant make a request in WolframAlpha API
                 call_wolframalpha(self.controller.latest_voice_transcript)
 
-        # Check for shutdown
         self.controller.shutdown_check()
