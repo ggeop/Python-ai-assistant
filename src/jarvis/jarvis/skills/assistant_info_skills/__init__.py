4ec66bf04a9a4042568af1ec540f070241625eb8
geo <geo>
2019-06-25 22:58:58 +0300

    Update assistant response.

--- src/jarvis/jarvis/skills/assistant_info_skills/__init__.py
+++ src/jarvis/jarvis/skills/assistant_info_skills/__init__.py
@@ -15,7 +15,7 @@ def assistant_check(**kargs):
 def _create_skill_response(response):
     for skill_id, skill in enumerate(skills_registry.BASIC_SKILLS.values()):
         response = response + '{0}) '.format(skill_id + 1) + skill['description'] + '\n'
-    return  response
+    return response
 
 
 def tell_the_skills(**kwargs):
@@ -24,12 +24,6 @@ def tell_the_skills(**kwargs):
     """
     response_base = 'I can do the following: \n\n'
     response = _create_skill_response(response_base)
-
-    response_utils.assistant_response("xmm")
-    response_utils.assistant_response("Good question..")
-    time.sleep(1)
-    response_utils.assistant_response("I can do a lot of things..")
-    time.sleep(1)
     response_utils.assistant_response(response)
 
 
