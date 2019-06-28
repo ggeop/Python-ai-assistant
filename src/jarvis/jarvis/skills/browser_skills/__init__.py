7a6b2c103fc88a836d8dab7ca35de5bbf324f378
geo <geo>
2019-06-29 02:20:38 +0300

    Add MIT licence in every module.

--- src/jarvis/jarvis/skills/browser_skills/__init__.py
+++ src/jarvis/jarvis/skills/browser_skills/__init__.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2019 Georgios Papachristou
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import wikipedia
 import requests
 import logging
@@ -5,6 +27,7 @@ import time
 import re
 import urllib.request
 import subprocess
+
 from bs4 import BeautifulSoup as bs
 
 from jarvis.core.response import assistant_response
@@ -46,7 +69,6 @@ def open_in_youtube(tag, voice_transcript, **kwargs):
     :param tag: string (e.g 'open')
     :param voice_transcript: string (e.g 'open in youtube tdex')
     """
-    # TODO:  Replace with YOUTUBE API
     reg_ex = re.search(tag + ' ([a-zA-Z]+)', voice_transcript)
     try:
         if reg_ex:
