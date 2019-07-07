ec0cfb1b9fea6977bf7586075ab744173fd01609
geo <geo>
2019-07-07 19:08:46 +0300

    update language processing

--- src/jarvis/jarvis/utils/language_processing.py
+++ src/jarvis/jarvis/utils/language_processing.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2019 Georgios Papachristou
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
+# of this software and associated documentation files (the 'Software'), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
@@ -12,7 +12,7 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -39,7 +39,7 @@ def create_parts_of_speech(text):
 
 
 def is_question_with_modal(parts_of_speech):
-    grammar = "QS: {<MD><PRP><VB>}"
+    grammar = 'QS: {<MD><PRP><VB>}'
     cp = nltk.RegexpParser(grammar)
     result = cp.parse(parts_of_speech)
     for subtree in result.subtrees():
@@ -48,10 +48,9 @@ def is_question_with_modal(parts_of_speech):
 
 
 def is_question_with_inversion(parts_of_speech):
-    grammar = "QS: {<VBP><PRP>}"
+    grammar = 'QS: {<VBP><PRP>}'
     cp = nltk.RegexpParser(grammar)
     result = cp.parse(parts_of_speech)
-    print('the result is', result)
     for subtree in result.subtrees():
         if subtree.label() in ['QS']:
             return True
@@ -78,26 +77,37 @@ def _extract_noun(parts_of_speech):
     return ' '
 
 
-# Construct Answer
+# Construct Response Body
 def create_response(sentence):
 
     parts_of_speech = create_parts_of_speech(sentence)
 
+    # Extract speech parts
+    verb = _extract_verb(parts_of_speech)
+    modal = _extract_modal(parts_of_speech)
+    noun = _extract_noun(parts_of_speech)
+
+    # Question type classification
     if is_question_with_modal(parts_of_speech):
-        logging.info("The user speech has a modal question")
-        verb = _extract_verb(parts_of_speech)
-        modal = _extract_modal(parts_of_speech)
-        noun = _extract_noun(parts_of_speech)
-        answer = 'Yes, I ' + modal + ' ' + verb + ' ' + noun
-        answer = re.sub("\s\s+", " ", answer)
-        return answer
-
+        logging.info('The user speech has a modal question')
+        answer = 'I ' + modal + ' ' + verb + ' ' + noun
     elif is_question_with_inversion(parts_of_speech):
-        logging.info("The user speech has an inverse question")
-        verb = _extract_verb(parts_of_speech)
-        noun = _extract_noun(parts_of_speech)
-        answer = 'Yes, I ' + ' ' + verb + ' ' + noun
-        answer = re.sub("\s\s+", " ", answer)
-        return answer
+        logging.info('The user speech has an inverse question')
+        answer = 'I ' + ' ' + verb + ' ' + noun
+    else:
+        logging.info('Unclassified question type')
+        answer = ''
+
+    return re.sub('\s\s+', ' ', answer)
 
 
+def create_positive_response(sentence):
+    positive_response = create_positive_response(sentence)
+    if positive_response:
+        return 'Yes, ' + positive_response
+
+
+def create_negative_response(sentence):
+    negative_response = create_positive_response(sentence)
+    if negative_response:
+        return 'No, ' + negative_response
