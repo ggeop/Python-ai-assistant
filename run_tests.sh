f4cfc3add7181c4cdcb061aa1adb23e20283b7f4
ggeop <georgios.papachristou0@gmail.com>
2021-10-10 15:51:55 +0300

    Fix mongodb service start

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+# --------------------------------
+# Activate Python virtual env
+# --------------------------------
 export PYTHONPATH="${PYTHONPATH}:./src/jarvis"
 source jarvis_virtualenv/bin/activate
-sudo service mongod start
+
+# --------------------------------
+# Start MongoDB service
+# --------------------------------
+sudo systemctl start mongodb
+
+# --------------------------------
+# Run unittests
+# --------------------------------
 python -m unittest discover -s ./src -p "*tests.py"
-sudo service mongod stop
+
+# --------------------------------
+# Stop MongoDB service
+# --------------------------------
+sudo systemctl stop mongodb
