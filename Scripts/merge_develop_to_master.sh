8ab8660210f308c95edfd4129b31cc1d9f59457e
ggeop <george.papachristou0@gmail.com>
2021-10-10 20:44:44 +0300

    Add auto taggin in Travis CD

--- Scripts/merge_develop_to_master.sh
+++ Scripts/merge_develop_to_master.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_BRANCH" != "develop" ]; then
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
     git config --global user.email "builds@travis-ci.com"
     git config --global user.name "Travis CI"
     git checkout master || exit
