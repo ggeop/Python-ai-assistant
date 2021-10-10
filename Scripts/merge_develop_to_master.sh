d67998c831b50ae531eb92eba775f0ff98a4f20d
ggeop <george.papachristou0@gmail.com>
2021-10-10 20:40:18 +0300

    Add auto taggin in Travis CD

---
+++ Scripts/merge_develop_to_master.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_BRANCH" != "develop" ]; then
+    git config --global user.email "builds@travis-ci.com"
+    git config --global user.name "Travis CI"
+    git checkout master || exit
+    git merge "$TRAVIS_COMMIT" || exit
+    git push -q https://$GITHUB_TOKEN@github.com/ggeop/Python-ai-assistant
+fi
