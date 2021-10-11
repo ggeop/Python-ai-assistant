1e3f873f80bb776b2213559e6ce34c7bd196b46c
ggeop <george.papachristou0@gmail.com>
2021-10-11 23:51:51 +0300

    Remove merge feature branch to develop

--- bin/deploy/new_release_auto_tagging.sh
+++ bin/deploy/new_release_auto_tagging.sh
@@ -8,3 +8,4 @@ git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 export GIT_TAG=Release_v.$TRAVIS_BUILD_NUMBER
 git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git push -q https://$GITHUB_TOKEN@github.com/ggeop/Python-ai-assistant --tags
