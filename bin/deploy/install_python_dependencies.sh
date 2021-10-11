09075a74b38e16e1c6ac5a4149f454e377d68b4f
ggeop <george.papachristou0@gmail.com>
2021-10-11 14:35:04 +0300

    Reduce job execution time

---
+++ bin/deploy/install_python_dependencies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#-----------------------------------
+# Install Python dependencies
+#-----------------------------------
+pip install -r requirements.txt
+pip install -U scikit-learn
---
+++ bin/deploy/install_system_dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#-----------------------------------
+# System dependencies installation
+#-----------------------------------
+sudo apt-get update && /
+sudo apt-get install build-essential && /
+sudo apt-get install python3-dev && /
+sudo apt-get install python3-setuptools && /
+sudo apt-get install python3-pip && /
+sudo apt-get install python3-venv && /
+sudo apt-get install portaudio19-dev python3-pyaudio python3-pyaudio && /
+sudo apt-get install libasound2-plugins libsox-fmt-all libsox-dev libxml2-dev libxslt-dev sox ffmpeg && /
+sudo apt-get install espeak && /
+sudo apt-get install libcairo2-dev libgirepository1.0-dev gir1.2-gtk-3.0  && /
+sudo apt install mongodb && /
+sudo apt-get install gnupg
