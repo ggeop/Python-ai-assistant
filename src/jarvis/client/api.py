28b0247957229a40942f4cbde0425ae9fa35b191
Zofia <zofi@xamh.de>
2021-10-28 13:05:53 +0200

    Created bare bone api manager

---
+++ src/jarvis/client/api.py
@@ -0,0 +1,17 @@
+import enum
+
+
+from enum import Enum
+
+class Status_code(Enum):
+    Success = 200
+    Server_Error = 500
+    Client_Error = 400
+
+
+class api_manager:
+    def __init__(self) -> None:
+        pass
+    
+    def post_user_input(transcript: str) -> Status_code:
+        pass
