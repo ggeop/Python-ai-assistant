f85d6ccd554e20338d23a7f7a6e0ee57a0d7d8d2
James Wynn <james@jameswynn.com>
2022-02-13 15:37:23 -0600

    Removed unnecessary import

--- src/jarvis/jarvis/utils/skills_database.py
+++ src/jarvis/jarvis/utils/skills_database.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from signal import pause
 import sqlite3
 import json
 from os.path import isdir, dirname
