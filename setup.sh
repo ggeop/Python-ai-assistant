e47e4483569e18b7d302b16f9e49f872501d7060
Juan Patrón <japatronvi@ciencias.unam.mx>
2023-03-25 00:25:17 -0600

    Create setup.sh
    Changes espeak for espeak-ng and updates instalation of mongodb for Ubuntu 22.04 LTS ("Jammy")

--- setup.sh
+++ setup.sh
@@ -33,13 +33,17 @@ sudo apt-get install python3-pip && /
 sudo apt-get install python3-venv && /
 sudo apt-get install portaudio19-dev python3-pyaudio python3-pyaudio && /
 sudo apt-get install libasound2-plugins libsox-fmt-all libsox-dev libxml2-dev libxslt-dev sox ffmpeg && /
-sudo apt-get install espeak && /
+sudo apt-get install espeak-ng && /
 sudo apt-get install libcairo2-dev libgirepository1.0-dev gir1.2-gtk-3.0  && /
-sudo apt install mongodb && /
-sudo apt-get install gnupg
+sudo apt-get install gnupg && /
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add - && /
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list && /
+sudo apt-get update && /
+sudo apt-get install -y mongodb-org
 
 # Reload local package database
 sudo apt-get update
+
 
 RESULT=$?
 if  [ $RESULT -eq 0 ]; then
